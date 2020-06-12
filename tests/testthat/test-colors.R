ca2ab2de33ea43bfcde5a801968bf9a29be8d449
Kara Woo <kara.woo@sagebase.org>
2020-06-12 12:59:44 -0700

    First two tests can be subsumed by latest one
    The regex for testing valid hex representations will also ensure that the
    strings begin with # and are the right number of characters

--- tests/testthat/test-colors.R
+++ tests/testthat/test-colors.R
@@ -1,23 +1,5 @@
 context("Colors")
 
-test_that("All colors start with a #", {
-  hash_start <- vapply(
-    sage_colors,
-    function(x) all(grepl("^#", x)),
-    FUN.VALUE = logical(1)
-  )
-  expect_true(all(hash_start))
-})
-
-test_that("All colors are 7 characters long", {
-  is_7 <- vapply(
-    sage_colors,
-    function(x) all(nchar(x) == 7),
-    FUN.VALUE = logical(1)
-  )
-  expect_true(all(is_7))
-})
-
 test_that("All colors are valid hex representations", {
   valid_hash <- vapply(
     sage_colors,
