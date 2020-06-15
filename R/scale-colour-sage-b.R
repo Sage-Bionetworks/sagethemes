81a8c8474dcc44431b6741b320820181460b808f
Kara Woo <kara.woo@sagebase.org>
2020-06-14 19:09:12 -0700

    Add binned scale
    Binning was only supported from 3.3.0, and binned_scale() will only be exported
    from 3.3.2, so we require the dev version for now. If the version is older, we
    go back to a continuous scale.

---
+++ R/scale-colour-sage-b.R
@@ -0,0 +1,89 @@
+#' Binned color scales
+#'
+#' These create a binned gradient of colors based on the high and low value of a
+#' given hue from within the list of Sage branded colors.
+#'
+#' @inheritDotParams ggplot2::scale_colour_gradient
+#' @inheritParams ggplot2::scale_colour_gradient
+#' @inheritParams sage_gradient_pal
+#' @export
+#' @rdname scale_sage_b
+#' @examples
+#' library("ggplot2")
+#'
+#' df_na <- data.frame(
+#'   value = seq(1, 20),
+#'   x = runif(20),
+#'   y = runif(20),
+#'   z1 = c(rep(NA, 10), rnorm(10))
+#' )
+#'
+#' ggplot(df_na, aes(x, y)) +
+#'   geom_point(aes(colour = z1)) +
+#'   scale_colour_sage_b(option = "apple")
+#'
+#' ggplot(faithfuld, aes(waiting, eruptions, fill = density)) +
+#'   geom_tile() +
+#'   scale_fill_sage_b(option = "powder")
+scale_colour_sage_b <- function(..., option = "royal", low = NULL, high = NULL,
+                                na.value = "grey50", guide = "colourbar",
+                                aesthetics = "colour") {
+  # TODO: replace with CRAN version after next ggplot2 release
+  if (packageVersion("ggplot2") < "3.3.1.9000") {
+    warning("Binned scales are only available in ggplot2 versions >= 3.3.1.9000. Reverting to continuous.") # nolint
+    return(
+      scale_colour_sage_c(
+        ...,
+        option = option,
+        low = low,
+        high = high,
+        na.value = na.value,
+        guide = guide,
+        aesthetics = aesthetics
+      )
+    )
+  }
+  ggplot2::binned_scale(
+    aesthetics,
+    "sage_b",
+    sage_gradient_pal(option = option, low = low, high = high),
+    na.value = na.value,
+    guide = guide,
+    ...
+  )
+}
+
+#' @export
+#' @rdname scale_sage_b
+scale_fill_sage_b <- function(..., option, low = NULL, high = NULL,
+                              na.value = "grey50", guide = "colourbar",
+                              aesthetics = "fill") {
+  # TODO: replace with CRAN version after next ggplot2 release
+  if (packageVersion("ggplot2") < "3.3.1.9000") {
+    warning("Binned scales are only available in ggplot2 versions >= 3.3.1.9000. Reverting to continuous.") # nolint
+    return(
+      scale_fill_sage_c(
+        ...,
+        option = option,
+        low = low,
+        high = high,
+        na.value = na.value,
+        guide = guide,
+        aesthetics = aesthetics
+      )
+    )
+  }
+  ggplot2::binned_scale(
+    aesthetics,
+    "sage_b",
+    sage_gradient_pal(option = option, low = low, high = high),
+    na.value = na.value,
+    guide = guide,
+    ...
+  )
+}
+
+#' @rdname scale_sage_b
+#' @export
+#' @usage NULL
+scale_color_sage_b <- scale_colour_sage_b
---
+++ tests/testthat/test-scale-colour-sage-b.R
@@ -0,0 +1,61 @@
+context("Binned color/fill scales")
+
+library("ggplot2")
+library("mockery")
+
+set.seed(200)
+df <- data.frame(
+  value = seq(1, 20),
+  x = runif(20),
+  y = runif(20),
+  z = c(rnorm(20))
+)
+
+test_that("scale_colour_sage_b creates binned color scale", {
+  p <- ggplot(df, aes(x, y)) +
+    geom_point(aes(colour = z))
+  p1 <- p + scale_colour_sage_b(option = "apple")
+  p2 <- p + scale_colour_sage_c(option = "apple")
+  p1_colors <- unique(layer_data(p1)$colour)
+  p2_colors <- unique(layer_data(p2)$colour)
+
+  expect_equal(length(p1_colors), 4)
+  # Plot with binned colors should have fewer unique colors than plot with
+  # continuous colors
+  expect_true(length(p1_colors) < length(p2_colors))
+})
+
+test_that("scale_fill_sage_b creates binned color scale", {
+  p <- ggplot(df, aes(x, y)) +
+    geom_tile(aes(fill = z))
+  p1 <- p + scale_fill_sage_b(option = "apple")
+  p2 <- p + scale_fill_sage_c(option = "apple")
+  p1_colors <- unique(layer_data(p1)$fill)
+  p2_colors <- unique(layer_data(p2)$fill)
+
+  expect_equal(length(p1_colors), 4)
+  # Plot with binned colors should have fewer unique colors than plot with
+  # continuous colors
+  expect_true(length(p1_colors) < length(p2_colors))
+})
+
+test_that("binned scale reverts to continuous on older ggplot2 versions", {
+  m <- mock("3.3.1")
+  stub(scale_colour_sage_b, "packageVersion", m)
+  set.seed(200)
+  expect_warning({
+    ggplot(df, aes(x, y)) +
+      geom_point(aes(colour = z)) +
+      scale_colour_sage_b()
+  })
+})
+
+# Visual tests -----------------------------------------------------------------
+
+test_that("Binned guide is created", {
+  dat <- data.frame(x = 1:5, y = 1:5)
+  p <- ggplot(dat, aes(x, y, fill = y)) +
+    geom_tile() +
+    scale_fill_sage_b(option = "fern")
+  vdiffr::expect_doppelganger("scale_fill_sage_b", p)
+})
